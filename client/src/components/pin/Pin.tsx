829d07b297ea51d618173b32bdc34793dcfe1f8a
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2024-07-11 13:35:49 +0100

    debugging map marker

--- client/src/components/pin/Pin.tsx
+++ client/src/components/pin/Pin.tsx
@@ -2,10 +2,13 @@ import { Link } from "react-router-dom";
 import { Marker, Popup } from "react-leaflet";
 import { CardProp } from "../../types/data";
 import "./pin.scss";
+import { LatLngExpression } from "leaflet";
 
 const Pin = ({ item }: CardProp) => {
+  const position: LatLngExpression = [+item.latitude, +item.longitude];
+
   return (
-    <Marker position={[+item.latitude, +item.longitude]}>
+    <Marker position={position}>
       <Popup>
         <div className="popupContainer">
           <img src={item.images[0]} alt={item.title} />
