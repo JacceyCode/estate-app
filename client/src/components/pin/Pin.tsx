f0c6dc8a5e0efdb1b4c447ea14e7154800e723bc
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2024-07-11 13:51:16 +0100

    2nd debug for map marker

--- client/src/components/pin/Pin.tsx
+++ client/src/components/pin/Pin.tsx
@@ -2,7 +2,20 @@ import { Link } from "react-router-dom";
 import { Marker, Popup } from "react-leaflet";
 import { CardProp } from "../../types/data";
 import "./pin.scss";
-import { LatLngExpression } from "leaflet";
+import L, { LatLngExpression } from "leaflet";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+const defaultIcon = L.icon({
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+
+L.Marker.prototype.options.icon = defaultIcon;
 
 const Pin = ({ item }: CardProp) => {
   const position: LatLngExpression = [+item.latitude, +item.longitude];
